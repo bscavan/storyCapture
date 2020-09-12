5e93d9920c7a2d29ff8e7853ef0628b1eac5a3f0
bcavanaugh <bcavanaugh@orbistechnologies.com>
2020-09-12 13:32:17 -0400

    Script now re-creates volumes based on the first new chapter found. The volume that includes that chapter and all of the following volumes are recreated.

--- rrStoryCapture.sh
+++ rrStoryCapture.sh
@@ -19,6 +19,21 @@
 # Params for overwriting the root url and search strings?
 # A param for changing the font-size style in the resulting HTML.
 
+# Returns the greater of two numbers provided.
+max () {
+	a=$1
+	b=$2
+	output=$(( a > b ? a : b ))
+	echo $output
+	return $output
+}
+
+decimal_divide () {
+	numerator=$1
+	denominator=$2
+	RESULT=$((${numerator}00/$denominator))
+	echo "${RESULT:0:-2}.${RESULT: -2}"
+}
 
 # This normally starts at zero.
 # TODO: Make a step that counts the number of files in "chapters" and sets this value to the result...
@@ -30,6 +45,10 @@ tocFileName="final-latest";
 
 let index=$startChapter+1;
 chapterCount=$startChapter;
+
+# The first new chapter found that doesn't already exist within a story's chapters directory. Usually set to 0 when the first chapter is downloaded, but on subsequent runs this will be a higher number. Used to prevent unnecessary work when generating volumes.
+let firstNewChapter=0
+((firstNewChapter = 0 - 1))
 
 toc_LinkRoot="https://www.royalroad.com"
 toc_Id=$1
@@ -71,6 +90,9 @@ sed -i 's/ //g' toc_working/toBeTrimmed
 # Trims off the first 2 lines. These are usually present, always garbage when they show up, and the good links always come in sets of threes, so we're safe to trim of them.
 sed -i 1,2d toc_working/toBeTrimmed
 
+# Trims off the last line. This is the always-present "reviews" link. Also, usable links always show up in sets of threes, so we're safe to remove it.
+sed -i '$ d' toc_working/toBeTrimmed
+
 echo "De-duplicate filtering ToC"
 uniq toc_working/toBeTrimmed > toc_working/$tocFileName
 
@@ -92,6 +114,10 @@ while read line; do
 		echo "\n"
 		echo "Chapter number ${index} already exists and is saved under the file name ${nextChapterName}. It will not be re-downloaded."
 	else
+		if [ $firstNewChapter -lt 0 ]; then
+			#echo "The first new chapter found was: $firstNewChapter, and it is now being set to $index"
+			firstNewChapter=$index
+		fi
 		# TODO: Make a listing of all of the exclusively new chapters. If they all exist at the end of the TOC, then we know they're all new. New chapters all get bundled into a single new volume.
 		# TODO: Make a command-line flag that controls whether the latest volume is completely recreated with the new chapters added or if a new volume is produced instead.
 			# Ex: If only 24 chapters were originally produced and now 35 exist the default behavior would be to completely recreate volume_1, however this flag would change the behavior so that chapters 25-35 would instead go into a new volume named volume_1.2
@@ -112,8 +138,36 @@ done < toc_working/$tocFileName
 # WARNING: When using --update mode take care to always use the same value for chaptersPerFile. If an update is being made this tool assumes existing volumes already have the specified number of files. If, for example, 800 chapters were saved with each volume holding 50 chapters, and update is run with a new setting for 60 chapters per file, the tool will believe chapter 801 should go into volume 13, not 17. As a result every volume from 1 to 12 will have 50 sequential chapters, for with volume 12 ending at chapter 600. Then volume 13 will skip ahead to chapter 780. Also, depending on the number of new chapters, later volumes may or may not be overwritten at all. Such as if only chapters 801 to 810 were newly available. In that instance volume 13 would contain chapters 780 to 810, and then volumes 14 to 16 would still contain chapters 651 to 800.
 	# Note to self "--update mode" is a planned feature, one that will preserve existing volumes when new chapters are found and only update the ones missing them/generate new volumes as needed.
 let chaptersPerFile=50;
+
+echo "/n"
+echo "The first new chapter found was $firstNewChapter"
+
+## TODO: Add a command-line flag to this script that, when provided, sets firstNewChapter to 1. That will force the recreation of all volumes.
+
+# If the variable firstNewChapter has not been set then no new chapters were found.
+if [ $firstNewChapter -lt 0 ]; then
+	echo "No new chapters were found. Therefore, no volumes need to be created."
+	exit 0;
+else
+	chaptersPerFile=$(max $chaptersPerFile 1)
+
+	# Divide the new chapter by the number of chapters per volume and preserve the decimal places.
+	startVolumeNumber=$(decimal_divide $firstNewChapter $chaptersPerFile)
+
+	# Round that number up to the nearest value.
+	startVolumeNumber=$(echo $startVolumeNumber | awk '{print int($1+0.5)}')
+
+	# Ensure that value is at least 1.
+	startVolumeNumber=$(max $startVolumeNumber 1)
+	echo "The first volume that contains new chapters will be $startVolumeNumber"
+
+	((startChapter = (startVolumeNumber - 1) * 50 + 1))
+	#startChapter=$(max $startChapter 1)
+	echo "The first chapter to include in that volume is $startChapter"
+fi
+
 let volumeNumber=$startVolumeNumber;
-let counter=0;
+let counter=1;
 
 mkdir $outputDir
 
@@ -135,17 +189,11 @@ chapterCount=$(ls ${storyName}chapters | wc -l)
 ## Converting chapterCount back into an integer here...
 chapterCount=$(($chapterCount + 0))
 
-if [ $startChapter -eq 0 ]; then
-	startChapter=1
-fi
-
-## TODO: Once the following loop is confirmed to work with the "ls | wc -l" command, cut out the previous solution for counting the chapters...
-
 echo "About to loop over all of the files in ./${storyName}chapters/, from chapter_ ${startChapter}.html to ${chapterCount}.html"
 for (( currentChapter=$startChapter; currentChapter<=$chapterCount; currentChapter++ ))
 do
 	file="./${storyName}chapters/chapter_${currentChapter}.html"
-	if (( $counter > $chaptersPerFile)); then
+	if (( $counter >= $chaptersPerFile)); then
 		echo "Reached chapter ${counter} out of ${chaptersPerFile} for volume number ${volumeNumber}. Finalizing volume now."
 
 		#Appends the ending HTML tags to the previous volume file.
@@ -175,7 +223,8 @@ do
 		/c/Program\ Files/wkhtmltopdf/bin/wkhtmltopdf.exe "${outputDir}/${storyName}volume_${volumeNumber}.html" volume_$volumeNumber.pdf
 
 		volumeNumber=$((volumeNumber+1));
-		counter=0;
+		# FIXME: Make counter start at 1, make this loop run while counter <= chaptersPerFile and remove the places where I'm logging counter + 1
+		counter=1;
 
 		#Appends the opening HTML tags to the new volume file.
 		echo $HTML_OPENING_TAGS $HTML_STYLE_TAG > "${outputDir}/${storyName}volume_${volumeNumber}.html"
@@ -187,7 +236,7 @@ do
 	
 	echo "Parsing $file and piping it into "${outputDir}/${storyName}volume_${volumeNumber}.html""
 	
-	echo "${HTML_CHAPTER_TITLE_OPENING_TAG}Chapter $((counter+1))/${chaptersPerFile} ${HTML_CHAPTER_TITLE_CLOSING_TAG}" >> "${outputDir}/${storyName}volume_${volumeNumber}.html"
+	echo "${HTML_CHAPTER_TITLE_OPENING_TAG}Chapter ${counter}/${chaptersPerFile} ${HTML_CHAPTER_TITLE_CLOSING_TAG}" >> "${outputDir}/${storyName}volume_${volumeNumber}.html"
 	
 	grep "<title>Chapter [0-9]" $file >> "${outputDir}/${storyName}volume_${volumeNumber}.html"
 
@@ -199,7 +248,7 @@ do
 	# Notice: this is a non-inclusive match (at least as far as line2 goes). This means we're successfully snipping out the advertisements element on line2!
 	# We are appending the found text to the current volume file
 	if [ -z $line1 ] || [ -z $line2 ]; then 
-		echo "failed to parse content of chapter number $((counter+1))."  >> "${outputDir}/${storyName}volume_${volumeNumber}.html"
+		echo "failed to parse content of chapter number $counter."  >> "${outputDir}/${storyName}volume_${volumeNumber}.html"
 	else
 		cat $file | tail -n +$line1 | head -n $((line2-line1)) >> "${outputDir}/${storyName}volume_${volumeNumber}.html"
 	fi
